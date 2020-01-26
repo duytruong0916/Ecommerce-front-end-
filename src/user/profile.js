9b1af6e1cf14e00475dbc576e6aa08eb0309e7d4
Duy Truong <duytruong169@gmail.com>
2020-01-26 17:01:04 -0600

    Revert "Revert "NEW UPDATE""
    This reverts commit 39368086b6a9a766c22425361e0357296bb32769.

--- src/user/profile.js
+++ src/user/profile.js
@@ -1,68 +1,184 @@
 import React, { useState, useEffect } from 'react';
 import Layout from '../core/Layout';
 import { connect } from 'react-redux';
-import { Redirect} from 'react-router-dom'
+import { states } from '../core/UsState';
+import { Redirect, Link} from 'react-router-dom'
 import { read, update, updateUserInLocalStorage } from '../redux-store/actions/user';
 const Profile = (props) => {
-    const [values, setvalues] = useState({
-        name: '',
-        email: '',
+    const [data, setdata] = useState({
+        firstname: '',
+        lastname: '',
         password: '',
         confirmpassword: '',
-        error: '',
+        addressNumber: '',
+        city: '',
+        state: '',
+        email: '',
+        phone: '',
+        zipcode: '',
+        error: false,
+        success: false,
+        errornotmatch: false,
         success: false
     })
-    const { name, email, password, confirmpassword,error, success } = values;
+    const { firstname, lastname, success, addressNumber, city, state, email, phone, zipcode, password, confirmpassword, error, errornotmatch } = data;
+    const onChangeHandler = (name) => (e) => {
+        const value = e.target.value;
+        setdata({ ...data, [name]: value });
+    }
     const init = (userid) => {
         read(userid, props.user.token ).then(response=>{
             if(response.error){
-                setvalues({...values, error: true})
+                setdata({...data, error: response.error})
             }else{
-                setvalues({...values, email: response.email, name: response.name})
+                const {firstname, email,lastname,phone} = response;
+                ///const array = address.split(' ');
+                setdata({...data,  email, firstname, lastname,phone})
             }
         })
     }
-    const ClickSubmit = (e)=>{
+    const onSubmit = (e)=>{
         e.preventDefault();
-        update(props.match.params.userid, props.user.token, {name, email, password}).then((data)=>{
-            if(data.error){
-                console.log(data.error);
-            }else{
-                props.updateUser(data);
-                setvalues({...values, name:data.name, email: data.email, success:true})
-            }
-        })
+        if (!firstname || !lastname || !email || !password || !confirmpassword) {
+            setdata({ ...data, error: 'Missing required fields' });
+        } else if (password !== confirmpassword) {
+            setdata({ ...data, errornotmatch: 'Passwords do not match' })
+        } else{
+            let address = `${addressNumber}. ${city}, ${state}-${zipcode}`;
+            update(props.match.params.userid, props.user.token,  {firstname, email,lastname, address,phone, password})
+            .then((response)=>{
+                if(response.error){
+                    console.log(data.error);
+                }else{
+                    setdata({...data,success:true})
+                    props.updateUser(data);
+                }
+            })
+        }
 
     }
     const RedirectUser = (success)=>{
         if(success){
-            return <Redirect to= '/cart'/>
+            return <Redirect to= '/user/dashboard'/>
         }
     }
-    const handleChange = title => (e)=>{
-            setvalues({...values, error: false, [title]: e.target.value});
-    }
-    const ProfileUpdate =(name, email, password)=>{
+    const ProfileUpdate =()=>{
         return (
-            <form>
-                <div className ='form-group'>
-                    <label className ='text-muted'>Name: </label>
-                    <input type='text' onChange ={handleChange('name')}     className='text-input w-100 ml-4 mt-4' value={name}/>
+            <div>
+            <form onSubmit={onSubmit} onBlur={() => setdata({ ...data, error: '', errornotmatch: '', success: '' })}>
+                <div className='row'>
+                    <div className='col-md-6 col-12'>
+                        <div className='mt-4'>
+                            <div><span className="font-weight-bold">*First name:</span></div>
+                            <input
+                                className={`text-input w-100 ${error && !firstname ? 'missing-field' : ''}`}
+                                type='text'
+                                placeholder='First name'
+                                value={firstname}
+                                onChange={onChangeHandler('firstname')} />
+                        </div>
+                        <div className='mt-4'>
+                            <div><span className="font-weight-bold">*Last name:</span></div>
+                            <input
+                                className={`text-input w-100 ${error && !lastname ? 'missing-field' : ''}`}
+                                type='text'
+                                value={lastname}
+                                placeholder='Last name'
+                                onChange={onChangeHandler('lastname')} />
+                        </div>
+                        <div className='w-50 mt-4'>
+                            <div><span className="font-weight-bold">Phone:</span></div>
+                            <input
+                                className='text-input w-100'
+                                type="text"
+                                value={phone}
+                                placeholder='+1( - - -) - - - - - - -'
+                                onChange={onChangeHandler('phone')} />
+                        </div>
+                        <div className='mt-4'>
+                            <div><span className="font-weight-bold">Address:</span></div>
+                            <div>
+                                <input
+                                    className='text-input w-100'
+                                    type='text'
+                                    value={addressNumber}
+                                    placeholder='Address (OR PO BOX)'
+                                    onChange={onChangeHandler('addressNumber')} />
+                            </div>
+                            <div >
+                                <select onChange={onChangeHandler('state')} className='text-input w-50' value={state}>
+                                    <option className='small'>STATE</option>
+                                    {states.map((st, i) => (
+                                        <option key={i} value={st}>{st}</option>
+                                    ))}
+                                </select>
+                            </div>
+                            <div >
+                                <input
+                                    className='text-input w-100'
+                                    type='text'
+                                    value={city}
+                                    placeholder='City'
+                                    onChange={onChangeHandler('city')} />
+                            </div>
+                            <div >
+                                <input
+                                    className='text-input w-100'
+                                    type='text'
+                                    value={zipcode}
+                                    placeholder='Zipcode'
+                                    onChange={onChangeHandler('zipcode')} />
+                            </div>
+                        </div>
+                    </div>
+                    <div className='col-md-6 col-12'>
+                        <div className='mt-4'>
+                            <div><span className="font-weight-bold">*Email:</span></div>
+                            <input
+                                className={`text-input w-100 ${error && !email ? 'missing-field' : ''}`}
+                                type='email'
+                                placeholder='Email'
+                                value={email}
+                                onChange={onChangeHandler('email')} />
+                        </div>
+                        <div className='mt-4'>
+                            <div><span className="font-weight-bold">*Password:</span></div>
+                            <div>
+                                <input
+                                    className={`text-input w-100 ${error && !password || errornotmatch ? 'missing-field' : ''}`}
+                                    type='password'
+                                    value={password}
+                                    placeholder='password'
+                                    onChange={onChangeHandler('password')} />
+                            </div>
+                            <div className='mt-4'>
+                                <div><span className="font-weight-bold">*Confirm password:</span></div>
+                                <input
+                                    className={`text-input w-100 ${error && !confirmpassword || errornotmatch ? 'missing-field' : ''}`}
+                                    type='password'
+                                    value={confirmpassword}
+                                    placeholder='Confirm password'
+                                    onChange={onChangeHandler('confirmpassword')} />
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className ='form-group'>
-                    <label className ='text-muted'>Email: </label>
-                    <input type='email' onChange ={handleChange('email')}     className='text-input w-100 ml-4 mt-4' value={email}/>
+
+                <div className='text-center text-danger'>
+                    {error && (<div>{error}</div>)}
+                    {!error && errornotmatch && (<div>{errornotmatch}</div>)}
                 </div>
-                <div className ='form-group'>
-                    <label className ='text-muted'>Password: </label>
-                    <input type='password' onChange ={handleChange('password')}     className='text-input w-100 ml-4 mt-4' value={undefined}/>
+                <div className='text-center text-success'>
+                    {success && (<div>{success}</div>)}
                 </div>
-                <div className ='form-group'>
-                    <label className ='text-muted'>Confirm password: </label>
-                    <input type='password' onChange ={handleChange('confirmpassword')}     className='text-input w-100 ml-4 mt-4' value={undefined}/>
+                <div className='mt-4'>
+                    <button className="button-card mt-3 w-50 p-4">SUBMIT</button>
                 </div>
-                <button className ='button-card w-50' onClick= {ClickSubmit}>Submit</button>
+                <Link to= '/user/dashboard'>
+                    <button className="button-card mt-3 w-50 p-4 mb-5">GO BACK</button>
+                </Link>
             </form>
+        </div>
         )
     }
     useEffect(() => {
@@ -71,8 +187,8 @@ const Profile = (props) => {
 
     return (
         <div className='profile-wrapper'>
-            <div className='page-header-title text-center'>PROFILE</div>
-            {ProfileUpdate(name,email, password)}
+            <div className='page-header-title text-center'>UPDATE PROFILE</div>
+            {ProfileUpdate()}
             {RedirectUser(success)}
         </div>
             
